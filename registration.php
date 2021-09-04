d176d7a62f09e1c0a080deb92abe20afc52ed888
DELORD Clément <cdelord@dawan.fr>
2021-09-04 03:23:28 +0200

    Empty folders

--- registration.php
+++ registration.php
@@ -6,4 +6,4 @@
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Magento/DAWAN', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Magento/Dawan', __DIR__);
