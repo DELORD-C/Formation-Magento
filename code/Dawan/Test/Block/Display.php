5000fd7e9579adc4c6d74944e42fb0b2c9ca4ebc
Clément <clement.delord@epitech.eu>
2021-09-06 12:06:53 +0200

    Update Theme + Module

---
+++ code/Dawan/Test/Block/Display.php
@@ -0,0 +1,9 @@
+<?php
+namespace Dawan\Test\Block;
+class Display extends \Magento\Framework\View\Element\Template
+{
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
---
+++ code/Dawan/Test/Controller/Test/View.php
@@ -0,0 +1,9 @@
+<?php
+namespace Dawan\Test\Controller\Test;
+class View extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        die('test view');
+    }
+}
